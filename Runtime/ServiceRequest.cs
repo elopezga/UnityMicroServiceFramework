9c00b07820b907947f662701db8d869f8bbde648
Edgar Lopez-Garcia <edgarylop@gmail.com>
2021-03-14 16:42:26 -0700

    Add GetParameterValue method to ServiceRequest so its easier for users to get values passed in the request

--- Runtime/ServiceRequest.cs
+++ Runtime/ServiceRequest.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,11 +12,29 @@ namespace ByteSize.MSF
         public readonly string Parameters;
         public readonly string Body;
 
+        private Dictionary<string, string> keyedSerializedParameters = new Dictionary<string, string>();
+
         public ServiceRequest(string url, string parameters, string body)
         {
             Url = url;
             Parameters = parameters;
             Body = body;
+
+            CreateKeyedSerializedParameters(parameters);
+        }
+
+        public T GetParameterValue<T>(string parameterKey)
+        {
+            return (T)Convert.ChangeType(keyedSerializedParameters[parameterKey], typeof(T));
+        }
+
+        private void CreateKeyedSerializedParameters(string parameters)
+        {
+            parameters.Split('&')
+            .ToList()
+            .Select(parameterPair => parameterPair.Split('='))
+            .ToList()
+            .ForEach(parameterKvp => keyedSerializedParameters.Add(parameterKvp[0], parameterKvp[1]));
         }
     }
 }
